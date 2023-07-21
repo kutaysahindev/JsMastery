88060d0dff2bce254e2906459a89d05e8b01c699
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-07-21 23:10:56 +0300

    Day 1 - Fundamentals

---
+++ Day 1/assignments.js
@@ -0,0 +1,9 @@
+//1
+
+let country = "Turkey";
+let continent = "Ankara";
+let population = 85;
+
+console.log(country, continent, population);
+
+//2
---
+++ Day 1/script.js
@@ -0,0 +1,5 @@
+//1
+
+console.log("kuti");
+
+//2
