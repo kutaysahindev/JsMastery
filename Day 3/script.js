aafbcf43dcbd3b36691f95987e56c25719133a76
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-08-08 15:59:37 +0300

    Day 3 - Final commit

--- Day 3/script.js
+++ Day 3/script.js
@@ -1,3 +1,4 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// This section is mainly focused on soft skills and editor setup, file is ready for final commit.
