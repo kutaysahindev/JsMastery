5acafbf68160166779dbb771e1bc7b69dcfcbf12
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-08-18 23:11:02 +0300

    Day 7 - Update script.js

--- Day 7/script.js
+++ Day 7/script.js
@@ -1,1 +1,22 @@
-'use strict';
+"use strict";
+
+// Global Scope - Top Level Code
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age} years old.`;
+    console.log(output);
+  }
+
+  if (birthYear >= 1981 && birthYear <= 1996) {
+    console.log(`Oh, you are millenial, ${firstName}`);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = "Jonas";
+calcAge(1991);
+// printAge() function is not accessible in here.
