dcce6304d504dbe6e9c84d18666432e5c9ac3545
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2023-08-20 14:55:06 +0300

    Day 7 - Update script.js

--- Day 7/script.js
+++ Day 7/script.js
@@ -1,22 +1,36 @@
 "use strict";
 
 // Global Scope - Top Level Code
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
 
-  function printAge() {
-    const output = `You are ${age} years old.`;
-    console.log(output);
-  }
+//   function printAge() {
+//     const output = `You are ${age} years old.`;
+//     console.log(output);
+//   }
 
-  if (birthYear >= 1981 && birthYear <= 1996) {
-    console.log(`Oh, you are millenial, ${firstName}`);
-  }
+//   if (birthYear >= 1981 && birthYear <= 1996) {
+//     console.log(`Oh, you are millenial, ${firstName}`);
+//   }
 
-  printAge();
-  return age;
+//   printAge();
+//   return age;
+// }
+
+// const firstName = "Jonas";
+// calcAge(1991);
+// printAge() function is not accessible in here.
+
+// Hoisting: lets you to use some variables before they're declared
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted!");
 }
 
-const firstName = "Jonas";
-calcAge(1991);
-// printAge() function is not accessible in here.
+// So, in this example, since the initial value of var is undefined, it's a falsy value, and it triggers the if statement.
